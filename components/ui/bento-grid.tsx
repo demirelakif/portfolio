78d5edd0ebf4c2c3e672d43421b7b8e2c378923c
coblu3 <demirel.akif@hotmail.com>
2024-10-19 18:59:10 +0300

    techs

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -45,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{ background: "rgb(2,0,36);", backgroundColor: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);" }}
@@ -77,8 +77,29 @@ export const BentoGridItem = ({
         )}
 
         {id === 2 && (
-          <GlobeDemo/>
+          <GlobeDemo />
         )}
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-2 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-3">
+              {['React.js', 'Next.js', 'TypeScript'].map((tech, index) => (
+                <span key={index} className="py-2 lg:py-5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg bg-[#10123E]">{tech}</span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+            </div>
+
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-3">
+            <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              {['Python', 'React-Native', 'Express.js'].map((tech, index) => (
+                <span key={index} className="py-2 lg:py-5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg bg-[#10123E]">{tech}</span>
+              ))}
+
+            </div>
+            <div />
+          </div>
+        )}
+
 
         <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}
         >
